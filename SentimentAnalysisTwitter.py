e86e6647be8849aadb284fb3ca91b56b5fe1f11f
b-winter30 <55878118+b-winter30@users.noreply.github.com>
2020-03-19 11:52:03 +0000

    Added the Sentiment Analysis of tweets
    Took T's work on returning tweets and made them into a list, then ran a sentiment analysis on each tweet, compiled the overall score from all tweets, and returned them through the run() function.

---
+++ SentimentAnalysisTwitter.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+import re
+import nltk
+nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import tweepy
+
+# keys, tokens and secrets
+CONSUMER_KEY = "5uZBofueiS8jKVhKXnpfr2MTN"
+CONSUMER_SECRET = "gTI1aFRaSFEvCYwgxyJp6BNZywsavklgipwWzswo7wyl48SzGq"
+ACCESS_TOKEN = "1049322266583031808-Of422oa5DYZwYKlDbBxg7zYVeXjrj1"
+ACCESS_SECRET = "i2KsOw73ECIRqEx53QNfqv6KKNh5eSwyPcayN5xJhhHxQ"
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+api = tweepy.API(auth)
+
+# number of tweets to find
+no_tweets = 1000
+
+# Key words and remove retweets
+new_search = "oil, price, WTI" + " -filter:retweets"
+
+
+def twitterAPI(api, no_tweets, new_search):
+    # collect tweets
+    tweets = tweepy.Cursor(api.search, q=new_search, lang="en").items(no_tweets)
+
+    # collect data from tweets
+    user_locs = [
+        [
+            tweet.text,
+            tweet.user.screen_name,
+            tweet.user.location,
+            tweet.favorite_count,
+            tweet.retweet_count,
+        ]
+        for tweet in tweets
+    ]
+    df = pd.DataFrame(
+        data=user_locs,
+        columns=["text", "user", "location", "like count", "retweet count"],
+    )
+    return df
+
+###
+def run():
+    #Call the twitter method and store all tweets in a list
+    StringArray = twitterAPI(api, no_tweets, new_search)['text'].values.tolist()
+
+    #Remove punctuation
+    for eachString in twitterAPI(api, no_tweets, new_search)['text']:
+        result = re.sub(r'[^a-zA-Z]', " ", eachString)
+        StringArray.remove(eachString)
+        StringArray.append(result)
+
+    #print (StringArray)
+    sid = SentimentIntensityAnalyzer()
+    overallScore = 0
+    for eachString in StringArray:
+        ss = sid.polarity_scores(eachString)
+        #Remove the comment below to find what the text in the articles consists of
+        #print (eachString, " has a score of ", ss['compound'])
+        overallScore += ss['compound']
+
+    print ("Overall score: ", overallScore / len(StringArray))
+    return overallScore
+
+run()
