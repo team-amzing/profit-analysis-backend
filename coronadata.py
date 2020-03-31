7bc0270d6537b351dee3cd254b833ffe45dc992b
Tonicha <t.m.crook15@outlook.com>
2020-03-31 12:29:20 +0100

    Coronavirus data normalised to show effect on stock

---
+++ coronadata.py
@@ -0,0 +1,19 @@
+from datetime import date
+import pandas as pd
+import requests
+
+from get_data.get_data import call_api
+
+# Date from the day before the outbreak in italy
+outbreakDate = date(2020, 2, 21)
+today = date.today()
+days = (today - outbreakDate).days
+print(days)
+
+# Collect prices and normalise to see the overall effect on the stock
+histprices = call_api(days)
+df = pd.DataFrame(histprices)
+dfs = [df.set_index("Date")]
+histpriceNormalised = pd.concat(dfs, axis=1)
+histpriceNormalised = histpriceNormalised / histpriceNormalised.iloc[0]
+
