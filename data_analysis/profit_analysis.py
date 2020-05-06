40a93841f1ded2ee158ccf1da695c7f1b1d655ab
Ben Winter <eeu60d@bangor.ac.uk>
2020-05-06 15:23:09 +0100

    Merge conflicts...

--- data_analysis/profit_analysis.py
+++ data_analysis/profit_analysis.py
@@ -8,14 +8,7 @@ def sell_today(value_today, value_tomorrow, no_units, daily_cost):
     """Returns a boolean value for to indicate whether to sell today."""
     profit_today = value_today * no_units
     profit_tomorrow = (value_tomorrow * no_units) - daily_cost
-<<<<<<< HEAD
-
     return profit_today > profit_tomorrow
-=======
-
-    return profit_today > profit_tomorrow
-
->>>>>>> db71eb3c1b603912253640000363a15d5be86791
 
 def get_predictions(model, n_days, n_predictions, current_value, no_units, daily_cost):
     """Returns the predictions from a given model and a comparison between the best predicted value and
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 """Script to run the ARIMA analysis and calculate a given number of predictions
 and the profit margin for that day, exported as a .npy file."""
-<<<<<<< HEAD
 import sys
 import os
 from numpy import save
@@ -16,7 +15,6 @@ file = os.path.join(THIS_FOLDER, 'get_data/macrotrends_data.csv')
 
 #Add todays oil price data to the csv
 addTodaysDateToMacrotrends(file)
-=======
 
 from datetime import date
 
@@ -24,7 +22,6 @@ from data_analysis.price_prediction import model_arima
 from data_analysis.profit_analysis import get_predictions
 from get_data.get_data import get_current_value
 from get_data.graphs_for_server import plot_to_file
->>>>>>> db71eb3c1b603912253640000363a15d5be86791
 
 # Model choice for analysis
 MODEL = model_arima
@@ -37,10 +34,6 @@ PREDICTED_DAYS = 9
 UNITS = 750000
 COST = 30000
 
-#Constants for vessel information
-UNITS = 750000
-COST = 30000
-
 # URL for scraping the current value
 URL = "https://markets.businessinsider.com/commodities/oil-price?type=wti"
 
@@ -49,28 +42,18 @@ VALUE = get_current_value(URL)
 
 date_today = date.today()
 
-<<<<<<< HEAD
 sell_today, predictions = get_predictions(MODEL, TRAINING_DAYS, PREDICTED_DAYS, VALUE, UNITS, COST)
-=======
-sell_today, predictions = get_predictions(
-    MODEL, TRAINING_DAYS, PREDICTED_DAYS, VALUE, UNITS, COST
-)
 
 # Generate a plot and table for the server
 plot_to_file("projection", predictions, sell_today, VALUE, PREDICTED_DAYS)
->>>>>>> db71eb3c1b603912253640000363a15d5be86791
 
 html_string = f"""
     <h1>WTI Oil Price Prediction for {date_today}</h1>
     <h2>Oil price today: {VALUE}</h2>
     {predictions.to_html()}
-<<<<<<< HEAD
-    <h2>Should you sell today? {sell_today}</h2>"""
-=======
     <h2>Should you sell today? {sell_today}</h2>
     <img src="projection.svg" height="500" width ="265" alt="Projection plot missing" >
 """
->>>>>>> db71eb3c1b603912253640000363a15d5be86791
 
 with open("index.html", "w") as file:
     file.write(html_string)
