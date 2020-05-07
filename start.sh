c62894f4b6d5870283ce4ac38a8d6dec5632b8aa
hstewart93 <harriet.stt@gmail.com>
2020-05-07 13:21:48 +0100

    Add matplotlib to Pipfile, copy svg over to nginx html location.

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ nginx
 while [ "0" = "0" ] ; do
     rm /usr/share/nginx/html/index.html
     cp /code/index.html /usr/share/nginx/html/.
+    cp /code/projection.svg /usr/share/nginx/html/.
     sleep 60
 
 done
