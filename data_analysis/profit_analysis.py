4487267239d85668d144536b3b492792759089ce
corcor27 <cory13579@googlemail.com>
2020-06-02 16:57:19 +0100

    Update profit_analysis.py

--- data_analysis/profit_analysis.py
+++ data_analysis/profit_analysis.py
@@ -6,11 +6,18 @@ for each prediction.
 
 def sell_today(value_today, value_tomorrow, no_units, daily_cost, error_tomorrow):
     """Returns a boolean value for to indicate whether to sell today."""
-    cost_today = int(value_today + error_tomorrow)
+    confidence = 0.8 # 80%
+    profit_today = value_today * no_units
     max_cost_tomorrow = int(value_tomorrow + error_tomorrow)
     min_cost_tomorrow = int(value_tomorrow - error_tomorrow)
-    print(max_cost_tomorrow, min_cost_tomorrow)
-    profit_today = value_today * no_units
+    difference = max_cost_tomorrow - min_cost_tomorrow
+    count = 0
+    for ii in range(min_cost_tomorrow, max_cost_tomorrow + 1):
+        profit_tomorrow = cost(no_units, (min_cost_tomorrow + ii), daily_cost)
+        if profit_tomorrow > profit_today:
+            count += 1
+    confidence_value = count/difference
+    if confidence_value > confidence:
     profit_tomorrow = (value_tomorrow * no_units) - daily_cost
     return profit_today > profit_tomorrow
 
@@ -25,3 +32,7 @@ def get_predictions(model, n_days, n_predictions, current_value, no_units, daily
     error_tomorrow = predictions['error'].iloc[0]
     return sell_today(current_value, next_value, no_units, daily_cost, error_tomorrow), predictions
 
+def cost(no_units, value, daily_cost):
+    profit = (value * no_units) - daily_cost
+    return profit
+    
