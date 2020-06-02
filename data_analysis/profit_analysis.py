16b3b38f2a7ffeb25d954211ef7c70905e9caea8
corcor27 <cory13579@googlemail.com>
2020-06-02 16:21:18 +0100

    Update profit_analysis.py

--- data_analysis/profit_analysis.py
+++ data_analysis/profit_analysis.py
@@ -4,8 +4,11 @@ for each prediction.
 """
 
 
-def sell_today(value_today, value_tomorrow, no_units, daily_cost):
+def sell_today(value_today, value_tomorrow, no_units, daily_cost, error_today):
     """Returns a boolean value for to indicate whether to sell today."""
+    max_profit_today = (value_today + error_today)*no_units
+    min_profit_today = (value_today - error_today)*no_units
+    
     profit_today = value_today * no_units
     profit_tomorrow = (value_tomorrow * no_units) - daily_cost
     return profit_today > profit_tomorrow
@@ -18,5 +21,6 @@ def get_predictions(model, n_days, n_predictions, current_value, no_units, daily
     next_value = predictions.predicted_value[0]
     todays_error = 10 * covid_today
     predictions['error'] = predictions['error'] * todays_error
-    return sell_today(current_value, next_value, no_units, daily_cost), predictions
+    error_today = predictions['error'].iloc
+    return sell_today(current_value, next_value, no_units, daily_cost, error_today), predictions
 
