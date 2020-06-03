99adf1dbeddf18d200b3d7b38444f8b094e74fc6
Thomas Spriggs <thomas.e.spriggs@gmail.com>
2020-06-03 15:29:33 +0100

    removed main() from test_sell_today(), also added the arguements daily_cost and no_days

--- test_sell_today.py
+++ test_sell_today.py
@@ -2,11 +2,6 @@ import pytest
 from data_analysis.profit_analysis import sell_today
 
 
-@pytest.mark.parametrize("a,b,expect", [(0.1, 100, 0), (100, 0.1, 1)])
-def tests_sell_today(a, b, expect):
-    assert sell_today(a, b, 1, 1) == expect
-
-
-def main():
-    tests_sell_today()
-    return
+@pytest.mark.parametrize("a,b,c,d,expect", [(0.1, 100, 1, 1, 0), (100, 0.1, 1, 1, 1)])
+def tests_sell_today(a, b, c, d, expect):
+    assert sell_today(a, b, c, d) == expect
