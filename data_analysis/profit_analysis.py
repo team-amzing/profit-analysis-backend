0b8e46d8d9014495ef0c7782f7797553834c3939
Ben Winter <eeu60d@bangor.ac.uk>
2020-06-05 20:13:34 +0100

    Cory: Implemented Covid code to the lower error rate.

--- data_analysis/profit_analysis.py
+++ data_analysis/profit_analysis.py
@@ -4,17 +4,39 @@ for each prediction.
 """
 
 
-def sell_today(value_today, value_tomorrow, no_units, daily_cost):
+def sell_today(value_today, value_tomorrow, no_units, daily_cost, error_tomorrow_upper,
+               error_tomorrow_lower):
     """Returns a boolean value for to indicate whether to sell today."""
+    confidence = 0.8 # 80%
     profit_today = value_today * no_units
-    profit_tomorrow = (value_tomorrow * no_units) - daily_cost
-    return profit_today > profit_tomorrow
+     
+    max_cost_tomorrow = error_tomorrow_upper
+    min_cost_tomorrow = error_tomorrow_lower
+    difference = max_cost_tomorrow - min_cost_tomorrow
+    count = 0
+     
+    for ii in range(int(min_cost_tomorrow), int(max_cost_tomorrow + 1)):
+        profit_tomorrow = cost(no_units, (min_cost_tomorrow + ii), daily_cost)
+        if profit_tomorrow > profit_today:
+            count += 1
+    confidence_value = count/difference
+    return confidence_value < confidence
 
 
-def get_predictions(model, n_days, n_predictions, current_value, no_units, daily_cost):
+def get_predictions(model, n_days, n_predictions, current_value, no_units, daily_cost, covid_today):
     """Returns the predictions from a given model and a comparison between the best predicted value and
     the current value."""
     predictions = model(n_days, n_predictions, current_value)
     next_value = predictions.predicted_value[0]
-    return sell_today(current_value, next_value, no_units, daily_cost), predictions
+    todays_error = covid_today
+    predictions['error_lower'] = predictions['error_lower'] * todays_error
+    error_tomorrow_upper = predictions['error_upper'].iloc[0]
+    error_tomorrow_lower = predictions['error_lower'].iloc[0]
+    return sell_today(current_value, next_value, no_units, daily_cost, error_tomorrow_upper,
+                      error_tomorrow_lower), predictions
+
+
+def cost(no_units, value, daily_cost):
+    profit = (value * no_units) - daily_cost
+    return profit
 
